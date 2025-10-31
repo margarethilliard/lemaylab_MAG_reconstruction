b984910527cc3b3adb861472a822b7bfb91856c5
Margaret Hilliard <53877776+margarethilliard@users.noreply.github.com>
2025-10-31 11:12:26 -0400

    Update submit_snakefile.sh for conda and snakemake

--- scripts/submit_snakefile.sh
+++ scripts/submit_snakefile.sh
@@ -7,15 +7,19 @@
 
 #run this scripts in running directory (usually one directory above /scripts)
 
-source ~/.bashrc
+module load conda/latest
+cd /path/to/project/dir
+source $(conda info --base)/etc/profile.d/conda.sh
 conda activate snakemake_env
 
+# debugging the logfile for conda init issues -- should version info in logfile(s) 
+which snakemake
+snakemake --version
+
+echo "Conda executable: $CONDA_EXE"
+echo "Conda level: $CONDA_SHLVL"
+
 # use-envmodules is needed since I have some programs as modules, not in a conda environment
-# we should move away from this approach soon, and load everything into conda environments 
-snakemake -s scripts/Snakefile \
-    --executor slurm \
-    --jobs 20 \
-    --use-conda \
-    --rerun-incomplete \
-    --printshellcmds \
-    --default-resources mem_mb=4096 runtime=600 slurm_partition=low
+# we should move away from this approach soon, and load everything into conda environments
+set -x
+snakemake -s scripts/Snakefile.py --use-conda --rerun-incomplete --executor slurm --jobs 3 --printshellcmds --directory /path/to/project/dir --default-resources mem_mb=4096 runtime=600 slurm_partition=low
